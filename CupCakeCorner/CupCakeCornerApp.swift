eb8fce3b3027ff4501864166e67e23f9bc38467b
Omnivrr <Omnivrr@gmail.com>
2023-04-28 17:03:38 +0300

    Initial Commit

---
+++ CupCakeCorner/CupCakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupCakeCornerApp.swift
+//  CupCakeCorner
+//
+//  Created by Bukhari Sani on 28/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct CupCakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
